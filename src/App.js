c0205d77ba4b1061759f4937af80f8638b4ac4d4
Yoel Zerbib <yoelzerbib7@gmail.com>
2021-01-20 16:37:01 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ function App() {
                   </div>
                 </Link>
               }
-              {/* <UploadForm /> */}
             </div>
           </Route>
         </Switch>
