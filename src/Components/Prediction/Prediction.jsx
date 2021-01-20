117ab2737605d6df669114e5bbd17e2d81d5c9dc
Yoel Zerbib <yoelzerbib7@gmail.com>
2021-01-20 14:39:11 +0200

    prediction page

--- src/Components/Prediction/Prediction.jsx
+++ src/Components/Prediction/Prediction.jsx
@@ -8,32 +8,45 @@ import {
   Col,
 } from "react-bootstrap";
 import pic from "./Team 12 (1).png";
-
+import './Prediction.css'
 import MapsModal from "../GoogleMap/mapsModal";
 
 const Prediction = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src={pic} className="mt-5"></Card.Img>
-          </Card>
-        </Col>
-        <Col>
-          <Card className="mt-5">
-            {" "}
-            <h1>test</h1>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col md="auto">
-          {" "}
-          <MapsModal />
-        </Col>
-      </Row>
-    </Container>
+    <div className='my-container'>
+      <div className='prediction-container'>
+        <Card className ='image-result'>
+          <img src="https://source.unsplash.com/random/300x300" alt=""/>
+        </Card>
+        <Card className='text-result'>
+          <h1>test</h1>
+          <div>hello</div>
+          <div>hello</div>
+          <div>hello</div>
+        </Card>
+      </div>
+      <MapsModal />
+
+    </div>
+    // <Container>
+    //   <Row>
+    //     <Col>
+    //       <Card>
+    //         <Card.Img variant="top" src={pic} className="mt-5"></Card.Img>
+    //       </Card>
+    //     </Col>
+    //     <Col>
+    //       <Card className="mt-5">
+    //         <h1>test</h1>
+    //       </Card>
+    //     </Col>
+    //   </Row>
+    //   <Row>
+    //     <Col md="auto">
+          // <MapsModal />
+    //     </Col>
+    //   </Row>
+    // </Container>
   );
 };
 
