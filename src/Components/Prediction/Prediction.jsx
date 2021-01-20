5fa0f1a2ab1233212b3dcd3fced413385a5e4d8d
Doron Assness <doronassness@Dorons-MBP.lan>
2021-01-20 14:09:49 +0200

    Prediction

--- src/Components/Prediction/Prediction.jsx
+++ src/Components/Prediction/Prediction.jsx
@@ -1,4 +1,12 @@
-import { Container, Card, Form, Button, Dropdown, Row } from "react-bootstrap";
+import {
+  Container,
+  Card,
+  Form,
+  Button,
+  Dropdown,
+  Row,
+  Col,
+} from "react-bootstrap";
 import pic from "./Team 12 (1).png";
 
 import MapsModal from "../GoogleMap/mapsModal";
@@ -6,10 +14,25 @@ import MapsModal from "../GoogleMap/mapsModal";
 const Prediction = () => {
   return (
     <Container>
-      <Card>
-        <Card.Img variant="top" src={pic} />
-      </Card>
-      <MapsModal />
+      <Row>
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={pic} className="mt-5"></Card.Img>
+          </Card>
+        </Col>
+        <Col>
+          <Card className="mt-5">
+            {" "}
+            <h1>test</h1>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col md="auto">
+          {" "}
+          <MapsModal />
+        </Col>
+      </Row>
     </Container>
   );
 };
