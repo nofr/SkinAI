dfc9970982a053d5ec166e54073d2414d0e75fd4
Doron Assness <doronassness@Dorons-MBP.lan>
2021-01-20 12:03:37 +0200

    Map Component

--- src/Components/Slides/Slides.jsx
+++ src/Components/Slides/Slides.jsx
@@ -3,37 +3,34 @@ import { Carousel } from "react-bootstrap";
 
 const Slides = () => {
   const carouselStyle = {
-    backgroundColor: 'whitesmoke',
-    zIndex: 0
-  }
+    backgroundColor: "whitesmoke",
+    zIndex: 0,
+  };
   return (
     <div>
-
-    <Carousel style={carouselStyle}>
-    <Carousel.Item className='about'>
-      <h1>Skin AI</h1>
-      <div>
-        The most common early sign of skin cancer is a change in the skin or a
-        lesion such as a beauty spot, freckle or mole.
-        <br /> Most skin cancers can be cured if caught early. Get to know the
-        symptoms of skin cancer to look out for. Sign up to Skin AI and get with
-        a certain accuracy a result before even going to the doctor !
-      </div>
-  </Carousel.Item>
-  <Carousel.Item className='about'>
-      <h1>About Us</h1>
-      <div>
-        The most common early sign of skin cancer is a change in the skin or a
-        lesion such as a beauty spot, freckle or mole.
-        <br /> Most skin cancers can be cured if caught early. Get to know the
-        symptoms of skin cancer to look out for. Sign up to Skin AI and get with
-        a certain accuracy a result before even going to the doctor !
-      </div>
-  </Carousel.Item>
-</Carousel>
+      <Carousel style={carouselStyle}>
+        <Carousel.Item className="about">
+          <h1>Skin AI</h1>
+          <div>
+            The most common early sign of skin cancer is a change in the skin or
+            a lesion such as a beauty spot, freckle or mole.
+            <br /> Most skin cancers can be cured if caught early. Get to know
+            the symptoms of skin cancer to look out for. Sign up to Skin AI and
+            get with a certain accuracy a result before even going to the doctor
+            !
+          </div>
+        </Carousel.Item>
+        <Carousel.Item className="about">
+          <h1>About Us</h1>
+          <div>
+            We are a group formed through ITC Fullstack and Data Science and
+            decided to solve an issue where we can determine weather you have
+            cancer by uploading a photo of a skin spot.
+          </div>
+        </Carousel.Item>
+      </Carousel>
     </div>
-
   );
-}
+};
 
 export default Slides;
